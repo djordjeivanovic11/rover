4f654e28038bd29098f6ae5d4bc56a6565461916
Beckett O'Brien <44044350+BeckettOBrien@users.noreply.github.com>
2026-02-11 18:45:15 -0500

    Zed imu/odom and gnss fusion for localization

---
+++ rover_navigation/launch/location.launch.py
@@ -0,0 +1,78 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+    gnss_driver = Node(
+        package='ublox_gps',
+        executable='ublox_gps_node',
+        name='gnss_driver',
+        output='screen',
+        parameters=[{
+            'device': '/dev/ttyACM0',
+            'frame_id': 'gps_link',
+            'baudrate': 38400,
+            'rate': 1.0,
+            'nav_rate': 1,
+            'enable_ppp': False,
+            'tmode3': 0,
+            'debug': 1,
+            'dgnss_mode': 3,  # RTK Fixed mode
+            'configure_gps': False,  # Don't reconfigure GPS
+        }],
+        remappings=[
+            ('fix', '/gps/fix'),
+            ('fix_velocity', '/gps/velocity')
+        ]
+    )
+
+    ekf_local_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_local_node',
+        output='screen',
+        parameters=[
+            PathJoinSubstitution([
+                FindPackageShare("rover_navigation"),
+                "config",
+                "localization.yaml",
+            ]), {'use_sim_time': False}]
+    )
+
+    navsat_transform_node = Node(
+        package='robot_localization',
+        executable='navsat_transform_node',
+        name='navsat_transform_node',
+        output='screen',
+        parameters=[
+            PathJoinSubstitution([
+                FindPackageShare("rover_navigation"),
+                "config",
+                "localization.yaml",
+            ]), {'use_sim_time': False}],
+        remappings=[
+            ('imu/data', '/Front_Zed/zed_node/imu/data')
+        ]
+    )
+
+    ekf_global_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_global_node',
+        output='screen',
+        parameters=[
+            PathJoinSubstitution([
+                FindPackageShare("rover_navigation"),
+                "config",
+                "localization.yaml",
+            ]), {'use_sim_time': False}]
+    )
+
+    return LaunchDescription([
+        gnss_driver,
+        ekf_local_node,
+        navsat_transform_node,
+        ekf_global_node,
+    ])
